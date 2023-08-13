d404073cd74289469db74d9540868ef4bb4e5a53
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2023-08-13 21:18:06 +0200

    Use modified chringel theme

---
+++ content/_index.md
@@ -0,0 +1,7 @@
+---
+author: Hans van Luttikhuizen-Ross
+title: Home
+date: 2023-08-13
+---
+
+Full-stack software engineer
---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    "postcss-import": {
+        path: ["./assets/"],
+    },
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
