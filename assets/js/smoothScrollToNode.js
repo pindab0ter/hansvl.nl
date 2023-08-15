2587db66f9d62c5e60e8239abfa2cf92f301b022
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2023-08-15 14:47:44 +0200

    Prevent NPE

--- assets/js/smoothScrollToNode.js
+++ assets/js/smoothScrollToNode.js
@@ -1,11 +1,15 @@
 function smoothScrollToNode(id) {
   var elementToView = document.getElementById(id);
   elementToView.scrollIntoView({
-    behavior: "smooth",
+    behavior: "smooth"
   });
 }
 
-document.getElementById("bouncing-arrow").onclick = (e) => {
-  smoothScrollToNode("featured-blog");
-  e.target.parentElement.remove();
-};
+const bouncingArrow = document.getElementById("bouncing-arrow");
+
+if (bouncingArrow !== null) {
+  bouncingArrow.onclick = (e) => {
+    smoothScrollToNode("featured-blog");
+    e.target.parentElement.remove();
+  };
+}
