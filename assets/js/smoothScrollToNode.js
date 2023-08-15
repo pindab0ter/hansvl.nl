7b09abf02b8cc7508d1dc4a79de7504453adb9e0
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2023-08-15 16:51:23 +0200

    Stop removing arrow when clicking it

--- assets/js/smoothScrollToNode.js
+++ assets/js/smoothScrollToNode.js
@@ -1,15 +1,11 @@
 function smoothScrollToNode(id) {
-  var elementToView = document.getElementById(id);
-  elementToView.scrollIntoView({
+  const elementToView = document.getElementById(id);
+  elementToView?.scrollIntoView({
     behavior: "smooth"
   });
 }
 
 const bouncingArrow = document.getElementById("bouncing-arrow");
-
-if (bouncingArrow !== null) {
-  bouncingArrow.onclick = (e) => {
-    smoothScrollToNode("featured-blog");
-    e.target.parentElement.remove();
-  };
-}
+bouncingArrow?.addEventListener("click", () => {
+  smoothScrollToNode("featured-blog");
+});
