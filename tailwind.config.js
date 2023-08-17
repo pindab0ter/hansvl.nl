a65c71b7807cf3632495e33ba6bda25730b151f1
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2023-08-17 19:44:34 +0200

    Fix and simplify color customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,3 @@
-const colors = {
-  ...require("tailwindcss/colors"),
-  amber: {
-    "50": "#fcf7ee",
-    "100": "#f5e7d0",
-    "200": "#e9cd9e",
-    "300": "#deaf6b",
-    "400": "#d69749",
-    "500": "#cc7832",
-    "600": "#b55c2a",
-    "700": "#974326",
-    "800": "#7b3625",
-    "900": "#662e21",
-    "950": "#3a150e"
-  },
-  blue: {
-    "50": "#ebf6ff",
-    "100": "#d1eaff",
-    "200": "#aedbff",
-    "300": "#76c7ff",
-    "400": "#35a6ff",
-    "500": "#077bff",
-    "600": "#0055ff",
-    "700": "#003cff",
-    "800": "#0031d7",
-    "900": "#0033b3",
-    "950": "#061e65"
-  },
-  gray: {
-    "50": "#f7f8fa",
-    "100": "#ebecf0",
-    "200": "#cfd1d2",
-    "300": "#adb1b3",
-    "400": "#84898c",
-    "500": "#696e71",
-    "600": "#5a5e60",
-    "700": "#4d4f51",
-    "800": "#323335",
-    "900": "#2b2b2b",
-    "950": "#232425"
-  }
-};
-
 const {
   borderColor,
   fontWeight,
@@ -60,12 +17,55 @@ module.exports = {
         body: ["Inter"],
         mono: ["JetBrains Mono"]
       },
+      colors: {
+        accent: {
+          light: {
+            "50": "#ebf6ff",
+            "100": "#d1eaff",
+            "200": "#aedbff",
+            "300": "#76c7ff",
+            "400": "#35a6ff",
+            "500": "#077bff",
+            "600": "#0055ff",
+            "700": "#003cff",
+            "800": "#0031d7",
+            "900": "#0033b3",
+            "950": "#061e65"
+          },
+          dark: {
+            "50": "#fcf7ee",
+            "100": "#f5e7d0",
+            "200": "#e9cd9e",
+            "300": "#deaf6b",
+            "400": "#d69749",
+            "500": "#cc7832",
+            "600": "#b55c2a",
+            "700": "#974326",
+            "800": "#7b3625",
+            "900": "#662e21",
+            "950": "#3a150e"
+          }
+        },
+        gray: {
+          "50": "#f7f8fa",
+          "100": "#ebecf0",
+          "200": "#cfd1d2",
+          "300": "#adb1b3",
+          "400": "#84898c",
+          "500": "#696e71",
+          "600": "#5a5e60",
+          "700": "#4d4f51",
+          "800": "#323335",
+          "900": "#2b2b2b",
+          "950": "#232425"
+        }
+      },
       spacing: {
         1.5: "0.375rem", // 6px
         1.6: "0.4375rem", // 7px
         2.1: "0.5625rem", // 9px
         3.2: "0.8125rem", // 16px
-        4.5: "1.125rem", // 8px
+        4.5: "1.125rem" // 8px
       },
       boxShadow: (theme) => ({
         "1-bottom": `inset 0 0 0 1px ${theme("colors.gray.400")}`
@@ -82,18 +82,18 @@ module.exports = {
             },
             pre: false,
             code: false,
-            'pre code': false,
-            'code::before': false,
-            'code::after': false
+            "pre code": false,
+            "code::before": false,
+            "code::after": false
           }
         },
         lg: {
           css: {
             pre: false,
             code: false,
-            'pre code': false,
-            'code::before': false,
-            'code::after': false,
+            "pre code": false,
+            "code::before": false,
+            "code::after": false
           }
         },
         dark: {
@@ -116,19 +116,10 @@ module.exports = {
             ol: { color: theme("colors.gray.200") },
             figcaption: { color: theme("colors.gray.300") },
             strong: { color: theme("colors.gray.200") },
-            span: { color: theme("colors.gray.200") },
+            span: { color: theme("colors.gray.200") }
           }
         }
       })
-    },
-    colors: {
-      black: colors.black,
-      white: colors.white,
-      gray: colors.gray,
-      accent: {
-        light: colors.blue,
-        dark: colors.amber
-      }
     },
     borderColor: (theme) => ({
       ...theme("colors")
@@ -152,7 +143,7 @@ module.exports = {
     animation: {
       blink: "blink 1s step-end infinite",
       bounce: "bounce 1s infinite"
-    },
+    }
   },
   plugins: [
     require("@tailwindcss/typography"),
