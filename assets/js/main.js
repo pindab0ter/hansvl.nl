796b0af2cb39547f423b842c083cdd5b0afd84dd
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2023-08-18 17:12:29 +0200

    Fix smooth scroll to recent blog section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 import copyHeadingLink from "./copyHeadingLink.js";
 import { setDarkMode, initialiseDarkModeListener } from "./darkMode.js";
+import smoothScrollToNode from "./smoothScrollToNode";
 
 /** Called by the link button next to prose headings. */
 window.copyHeadingLink = copyHeadingLink;
@@ -7,3 +8,6 @@ window.copyHeadingLink = copyHeadingLink;
 /** Called by the dark mode toggle button in the body. */
 window.setDarkMode = setDarkMode;
 initialiseDarkModeListener();
+
+/** Called by the bouncing arrow on the home page. */
+window.smoothScrollToNode = smoothScrollToNode;
--- assets/js/smoothScrollToNode.js
+++ assets/js/smoothScrollToNode.js
@@ -1,11 +1,6 @@
-function smoothScrollToNode(id) {
+export default function smoothScrollToNode(id) {
   const elementToView = document.getElementById(id);
   elementToView?.scrollIntoView({
     behavior: "smooth"
   });
 }
-
-const bouncingArrow = document.getElementById("bouncing-arrow");
-bouncingArrow?.addEventListener("click", () => {
-  smoothScrollToNode("featured-blog");
-});
