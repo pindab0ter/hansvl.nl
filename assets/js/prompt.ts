79be8685b3839eaf8d3a18e2e3a3b0945b8abd1d
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2024-04-08 12:12:06 +0200

    Refactor prompt script

--- assets/js/prompt.ts
+++ assets/js/prompt.ts
@@ -1,58 +1,73 @@
-export default function initialisePrompt() {
+export default function initialisePrompt(): void {
   document.addEventListener("DOMContentLoaded", () => {
-    const promptInput = document.getElementById("prompt-input") as HTMLSpanElement;
-    const promptBlur = document.getElementById("prompt-blur") as HTMLSpanElement;
+    const promptInput: HTMLSpanElement = document.getElementById("prompt-input");
+    const promptBlur: HTMLSpanElement = document.getElementById("prompt-blur");
+    const nav: HTMLElement = document.getElementsByTagName("nav")[0];
 
-    promptBlur.textContent = promptInput.textContent;
+    // Focus prompt input on page load for browsers that don't support autofocus on contenteditable elements.
     promptInput.focus();
 
-    // Mirror prompt input to prompt blur.
-    promptInput.addEventListener("input", () => {
-      promptBlur.textContent = promptInput.textContent;
-    });
-
-    // Move caret to end of prompt input when focused to 'resume' typing.
-    promptInput.addEventListener("focusin", () => {
-      const range = document.createRange();
-      const selection = window.getSelection();
-
-      // Move caret to end of prompt input.
-      range.setStart(promptInput, promptInput.childNodes.length);
-      range.collapse(false);
-      selection.removeAllRanges();
-      selection.addRange(range);
-    });
+    mirrorInputPromptToBlurredPrompt(promptInput, promptBlur);
+    moveCaretToEndOnFocus(promptInput);
+    listenForKeyboardInput(promptInput, promptBlur);
+    focusPromptOnClick(promptInput, nav);
   });
-
-  handleButtonPresses();
-  focusPromptOnClick();
 }
 
-function handleButtonPresses() {
+function mirrorInputPromptToBlurredPrompt(
+  promptInput: HTMLSpanElement,
+  promptBlur: HTMLSpanElement,
+): void {
+  promptInput.addEventListener("input", () => {
+    promptBlur.textContent = promptInput.textContent;
+  });
+}
+
+function moveCaretToEndOnFocus(promptInput: HTMLSpanElement): void {
+  promptInput.addEventListener("focusin", () => {
+    const range = document.createRange();
+    const selection = window.getSelection();
+
+    // Move caret to end of prompt input.
+    range.setStart(promptInput, promptInput.childNodes.length);
+    range.collapse(false);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  });
+}
+
+function listenForKeyboardInput(promptInput: HTMLSpanElement, promptBlur: HTMLSpanElement): void {
   /**
    * Handle enter key press to clear the prompt input.
    */
   document.addEventListener("keydown", (event: KeyboardEvent) => {
-    const promptInput = document.getElementById("prompt-input") as HTMLSpanElement;
-    const promptBlur = document.getElementById("prompt-blur") as HTMLSpanElement;
+    switch (event.key) {
+      // Clear prompt input
+      case "Enter":
+        event.preventDefault();
 
-    if (event.key == "Enter") {
-      event.preventDefault();
+        promptInput.textContent = "";
+        promptBlur.textContent = "";
+        break;
 
-      promptInput.textContent = "";
-      promptBlur.textContent = "";
-    }
-
-    if (event.key == "Escape") {
-      promptInput.blur();
+      // Remove focus from prompt input
+      case "Escape":
+        promptInput.blur();
+        break;
     }
   });
 }
 
-function focusPromptOnClick() {
-  const nav = document.getElementsByTagName("nav")[0];
-  nav.addEventListener("click", () => {
-    const promptInput = document.getElementById("prompt-input") as HTMLSpanElement;
+function focusPromptOnClick(promptInput: HTMLSpanElement, nav: HTMLElement): void {
+  /**
+   * Focus prompt input when clicking on the header.
+   */
+  nav.addEventListener("click", (event) => {
+    // Prevent focusing prompt input when clicking on a link.
+    if (event.target instanceof HTMLAnchorElement) {
+      return;
+    }
+
     promptInput.focus();
   });
 }
